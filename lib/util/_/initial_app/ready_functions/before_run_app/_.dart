a2269fad22e473ed79063a654cb2bcefaa37fe7b
june lee <melodysdreamj@gmail.com>
2024-05-20 09:56:46 +0900

    `

--- lib/util/_/initial_app/ready_functions/before_run_app/_.dart
+++ lib/util/_/initial_app/ready_functions/before_run_app/_.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import '../../../../../main.dart';
-
+import 'beamer_module/_.dart';
 
 Future<void> readyBeforeRunApp() async {
   if (_done) return;
   _done = true;
   
+  await Future.wait([
+    readyForBeamerModule(),
+  ]);
 
 
 }
